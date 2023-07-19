a085121b69dc912c946caa8c2d9cff93525d7c97
Your Name <sharunnd11@gmail.com>
2023-07-19 20:39:14 +0530

    image upload with the help of cloudinary done

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,13 @@
+const cloudinary = require("cloudinary").v2
+require("dotenv").config()
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_KEY, 
+    api_secret: process.env.CLOUD_SECRET_KEY
+  });
+
+  module.exports = {
+    cloudinary
+  }
